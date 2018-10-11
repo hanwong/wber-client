f7d68b0af5dedb1bb4db2add96fc4923118325a5
hanwong <hanationbear@gmail.com>
2018-10-11 23:47:01 +0900

    Router and Routes

--- src/Components/App/AppPresenter.tsx
+++ src/Components/App/AppPresenter.tsx
@@ -1,14 +1,51 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import PropTypes from "prop-types";
+import React from "react";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+
+import AddPlace from "../../Routes/AddPlace";
+import EditAccount from "../../Routes/EditAccount";
+import FindAddress from "../../Routes/FindAddress";
+import Home from "../../Routes/Home";
+import OutHome from "../../Routes/OutHome";
+import PhoneLogin from "../../Routes/PhoneLogin";
+import Places from "../../Routes/Places";
+import Ride from "../../Routes/Ride";
+import Settings from "../../Routes/Settings";
+import SocialLogin from "../../Routes/SocialLogin";
+import VerifyPhone from "../../Routes/VerifyPhone";
 
 interface IProps {
   isLoggedIn: boolean;
 }
 
 const AppPresenter: React.SFC<IProps> = ({isLoggedIn}) => (
-  isLoggedIn ? <span>you are in</span> : <span>you are out</span>
+  <BrowserRouter>
+    {isLoggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes/>}
+  </BrowserRouter>
 );
 
+const LoggedOutRoutes: React.SFC<IProps> = () => (
+  <Switch>
+    <Route path={'/'} exect={true} component={OutHome} />
+    <Route path={'/phone-login'} exect={true} component={PhoneLogin} />
+    <Route path={'/verfy-phone'} exect={true} component={VerifyPhone} />
+    <Route path={'/social-login'} exect={true} component={SocialLogin} />
+    <Redirect from={"*"} to={"/"} />
+  </Switch>
+);
+
+const LoggedInRoutes: React.SFC<IProps> = () => (
+  <Switch>
+    <Route path={'/'} exect={true} component={Home} />
+    <Route path={'/ride'} exect={true} component={Ride} />
+    <Route path={'/edit-account'} exect={true} component={EditAccount} />
+    <Route path={'/settings'} exect={true} component={Settings} />
+    <Route path={'/places'} exect={true} component={Places} />
+    <Route path={'/add-place'} exect={true} component={AddPlace} />
+    <Route path={'/find-address'} exect={true} component={FindAddress} />
+    <Redirect from={"*"} to={"/"} />
+  </Switch>
+);
 
 
 AppPresenter.propTypes = {
